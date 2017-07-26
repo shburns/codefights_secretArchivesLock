056c8bda0ad34742538db56a332416cc09c4cffe
BEEMO\shurns <shburns02@gmail.com>
2017-07-26 01:41:27 -0400

    Final submission. For real this time though... because I won't have time to change it before the competition ends.

--- secretArchivesLock.js
+++ secretArchivesLock.js
@@ -1,41 +1,25 @@
 function secretArchivesLock(lock, actions) {
-    //Reduce the instruction set, improve performance.
-    let reducedActions = actions.replace(/(L(L)+|(L|R)+L)/g, 'L')
-                                .replace(/(R(R)+|(L|R)+R)/g, 'R')
-                                .replace(/(U(U)+|(D|U)+U)/g, 'U')
-                                .replace(/(D(D)+|(U|D)+D)/g, 'D');
-    
     //Rotate the matrix 90 degrees clockwise
     const rotateRight = (lockState) => 
         lockState.reduceRight((newState, currRow) => {
             [...currRow].forEach((char, index) => 
-                newState[index] = (newState[index]) ? newState[index].concat(char) : char
+                newState[index] = (newState[index] || "").concat(char)
             );
             return newState;
         }, []);
     
     //Rotate the matrix 90 degrees counter-clockwise
-    const rotateLeft = (lockState) => 
-        lockState.reduce((newState, currRow) => {
-            [...currRow].forEach((char, index) => 
-                newState[index] = (newState[index]) ? newState[index].concat(char) : char
-            );
-            return newState;
-        }, []).reverse();
+    const rotateLeft = (lockState) => rotateRight(lockState.reverse()).reverse();
     
     //Shift cells in the current orientation left or right
-    const emptyCells = (char) => char == '.';
+    const emptyCells = (char) => char === '.';
     const occupiedCells = (char) => char != '.';
     
-    const shiftLeft = (lockState) =>
-        lockState.map((row) =>
-            [...row].filter(occupiedCells).concat([...row].filter(emptyCells)).join('')
-        );
-    
-    const shiftRight = (lockState) =>
-        lockState.map((row) => 
-            [...row].filter(emptyCells).concat([...row].filter(occupiedCells)).join('')
-        );
+    const shift = (lockState, order) => lockState.map((row) =>
+        [...row].filter(order[0]).concat([...row].filter(order[1])).join('')
+    );
+    const shiftLeft = (lockState) => shift(lockState, [occupiedCells, emptyCells]);
+    const shiftRight = (lockState) => shift(lockState, [emptyCells, occupiedCells]);
     
     //Define behavior for each action
     const executeAction = {
@@ -52,6 +36,12 @@ function secretArchivesLock(lock, actions) {
             return rotateLeft(newState);
         }
     };
+    
+    //Reduce the instruction set, improve performance.
+    let reducedActions = actions.replace(/(L(L)+|(L|R)+L)/g, 'L')
+                                .replace(/(R(R)+|(L|R)+R)/g, 'R')
+                                .replace(/(U(U)+|(D|U)+U)/g, 'U')
+                                .replace(/(D(D)+|(U|D)+D)/g, 'D');
 
     //Execute all actions in sequence
     let newLock = lock;
